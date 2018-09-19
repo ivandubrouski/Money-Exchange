b2e1203cdc9cb85f060864750ff659da275a3d8b
Ivan <ivan.dubrouski@gmail.com>
2018-09-19 18:14:03 +0300

    money-exchange[done]/ solution of the task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-// PLEASE DON'T change function name
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+
+	var amount={};
+	if(currency>10000){
+		return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+	} else if(currency<=0){
+		return {};
+	}
+
+	var coins={"H":50,"Q":25,"D":10,"N":5,"P":1};
+	for(var key in coins) {
+		var temp=(currency-currency%coins[key])/coins[key];
+		if (temp>0){
+			amount[key]=temp;
+			currency-=coins[key]*temp;
+		}
+	}
+	return amount;
 }
